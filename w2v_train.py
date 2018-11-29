ad67f2d760d199247d74a0dab5ed16e425145c8e
WangHexie <31768052+WangHexie@users.noreply.github.com>
2018-11-29 23:09:40 +0800

    able to save embedding

--- w2v_train.py
+++ w2v_train.py
@@ -1,10 +1,13 @@
-from tensorflow.keras import layers, Model, optimizers
-import pandas as pd
 import os
+
 import numpy as np
+import pandas as pd
+import keras
+from keras.utils import to_categorical
+from tensorflow.keras import layers, Model, optimizers
 
 
-def build_keras_model(cat_dims: list, attributes_dims: list, embedding_length=32, attr_embedding_length=8):
+def build_keras_model(cat_dims: list, attributes_dims: list, embedding_length=80, attr_embedding_length=8):
     """
 
     :param attr_embedding_length:
@@ -20,20 +23,21 @@ def build_keras_model(cat_dims: list, attributes_dims: list, embedding_length=32
     attributes_inputs = [layers.Input(shape=(1,), name="attributes_input{}".format(i)) for i in
                          range(len(attributes_dims))]
 
-    cat_embeddings = [layers.Embedding(input_length=1, input_dim=cat_dim, output_dim=embedding_length) for cat_dim in
-                      cat_dims]
+    cat_embeddings = [
+        layers.Embedding(input_length=1, input_dim=cat_dims[index], output_dim=embedding_length,
+                         name="cat_embedding_{}".format(index)) for index in range(len(cat_dims))]
 
-    attributes_embeddings = [layers.Embedding(input_length=1, input_dim=attr_dim, output_dim=attr_embedding_length) for
-                             attr_dim in
-                             attributes_dims]
+    attributes_embeddings = [
+        layers.Embedding(input_length=1, input_dim=attributes_dims[index], output_dim=attr_embedding_length,
+                         name="attr_embedding_{}".format(index)) for index in range(len(attributes_dims))]
 
-    embedded_cat = [layers.Activation("sigmoid")(cat_embeddings[i](cat_inputs[i])) for i in
+    embedded_cat = [layers.Activation("relu")(cat_embeddings[i](cat_inputs[i])) for i in
                     range(len(cat_dims))]
 
-    embedded_cat_next = [layers.Activation("sigmoid")(cat_embeddings[i](cat_next_inputs[i])) for i in
+    embedded_cat_next = [layers.Activation("relu")(cat_embeddings[i](cat_next_inputs[i])) for i in
                          range(len(cat_dims))]
 
-    embedded_attr = [layers.Activation("sigmoid")(attributes_embeddings[i](attributes_inputs[i])) for i in
+    embedded_attr = [layers.Activation("relu")(attributes_embeddings[i](attributes_inputs[i])) for i in
                      range(len(attributes_dims))]
 
     embedded_all = embedded_cat + embedded_cat_next + embedded_attr
@@ -41,9 +45,9 @@ def build_keras_model(cat_dims: list, attributes_dims: list, embedding_length=32
     flattened_cat = [layers.Flatten(name="flattened_{}".format(i))(embedded_all[i]) for i in range(len(embedded_all))]
 
     concatenate_layer = layers.Concatenate(axis=-1)(flattened_cat)
-    output = layers.Dense(1, activation="sigmoid", name="output")(concatenate_layer)
+    output = layers.Dense(2, activation="sigmoid", name="output")(concatenate_layer)
     model = Model(inputs=cat_inputs + cat_next_inputs + attributes_inputs, outputs=output)
-    model.compile(optimizer=optimizers.Adam(), loss="mean_squared_error")
+    model.compile(optimizer=optimizers.Adam(), loss="categorical_crossentropy", metrics=['mae', 'acc'])
     model.summary()
     return model
 
@@ -56,8 +60,9 @@ def load_data() -> [pd.DataFrame, pd.DataFrame]:
     #                        columns=["age_range", "gender"])]
 
 
-def batch_generator(data: [pd.DataFrame, pd.DataFrame], batch_size=64, negative_percent=0.5, windows_size=1,
-                    skip_num=2):
+def batch_generator(data: list, batch_size: int = 64, negative_percent: float = 0.8,
+                    windows_size: int = 1,
+                    skip_num: int = 2):
     """
 
     :param skip_num:
@@ -79,7 +84,7 @@ def batch_generator(data: [pd.DataFrame, pd.DataFrame], batch_size=64, negative_
         while batch_num < batch_size:
             if (index + windows_size + 1) > len(data[0]):  # start from head when reaching the end
                 index = windows_size
-                
+
             if true_num < true_needed:
                 batch_next = np.concatenate((b_data[index - windows_size:index],
                                              b_data[index + 1:index + 1 + windows_size]))
@@ -116,19 +121,41 @@ def batch_generator(data: [pd.DataFrame, pd.DataFrame], batch_size=64, negative_
             [('cat_input_{}'.format(i), batch[i]) for i in range(3)] + [('cat_next_input_{}'.format(i), batch[i + 3])
                                                                         for i in range(3)] +
             [('attributes_input{}'.format(i), batch[i + 6]) for i in range(2)]
-        ), dict([('output', label)]))
+        ), dict([('output', to_categorical(label))]))
         yield x
 
 
 def train():
-    batch_size = 64
+    batch_size = 128
     data = load_data()
     cat_dims = [len(set(data[0][name])) for name in ["seller_id", "cat_id", "brand_id"]]
     attr_dims = [len(set(data[1][name])) for name in ["age_range", "gender"]]
     model = build_keras_model(cat_dims, attr_dims)
-    model.fit_generator(batch_generator(data=data, batch_size=batch_size),
-                        steps_per_epoch=int(len(data[0]) / batch_size), epochs=10)
+    model.fit_generator(batch_generator(data=data, batch_size=batch_size, windows_size=4, skip_num=1),
+                        steps_per_epoch=int(len(data[0]) / batch_size), epochs=20)
+    model.save("model.h5")
+    return model
+
+
+def save_embedding(data: [pd.DataFrame, pd.DataFrame], model: Model, num_of_node_type=3):
+    """
+
+    :param data: data[0]:link data ,data[1]:user_info
+    :param model:
+    :param num_of_node_type:
+    :return:
+    """
+    data_values = data[0][["user_id", "seller_id", "cat_id", "brand_id"]].values.T
+    full_embedding = []
+    for i in range(num_of_node_type):
+        embedding = model.get_layer("cat_embedding_{}".format(i)).get_weights()
+        full_embedding.append(embedding[0])
+    np.save("embedding_w2v.npy", full_embedding)
+    return full_embedding
 
 
 if __name__ == '__main__':
-    train()
+    # train()
+    data = load_data()
+    model = keras.models.load_model("model.h5")
+    save_embedding(data, model, 3)
